89e0205f25265024d205059f6c39786a9c3640dd
mrx-arafat <arafatmrx@gmail.com>
2023-09-09 10:46:14 +0600

    dna

---
+++ dna.py
@@ -0,0 +1,95 @@
+
+mapping = {
+        
+'AAA':'a',
+'AAC':'b',
+'AAG':'c',
+'AAT':'d',
+'ACA':'e',
+'ACC':'f', 
+'ACG':'g',
+'ACT':'h',
+'AGA':'i',
+'AGC':'j',
+'AGG':'k',
+'AGT':'l',
+'ATA':'m',
+'ATC':'n',
+'ATG':'o',
+'ATT':'p',
+'CAA':'q',
+'CAC':'r',
+'CAG':'s',
+'CAT':'t',
+'CCA':'u',
+'CCC':'v',
+'CCG':'w',
+'CCT':'x',
+'CGA':'y',
+'CGC':'z',
+'CGG':'A',
+'CGT':'B',
+'CTA':'C',
+'CTC':'D',
+'CTG':'E',
+'CTT':'F',
+'GAA':'G',
+'GAC':'H',
+'GAG':'I',
+'GAT':'J',
+'GCA':'K',
+'GCC':'L',
+'GCG':'M',
+'GCT':'N',
+'GGA':'O',
+'GGC':'P',
+'GGG':'Q',
+'GGT':'R',
+'GTA':'S',
+'GTC':'T',
+'GTG':'U',
+'GTT':'V',
+'TAA':'W',
+'TAC':'X',
+'TAG':'Y',
+'TAT':'Z',
+'TCA':'1',
+'TCC':'2',
+'TCG':'3',
+'TCT':'4',
+'TGA':'5',
+'TGC':'6',
+'TGG':'7',
+'TGT':'8',
+'TTA':'9',
+'TTC':'0',
+'TTG':' ',
+'TTT':'.'
+
+	
+}
+
+
+
+encode_string = 'OME'
+
+decode_string = 'CGGTTGACGACAATCACACATAGAAAGTTGAGAATCACCATGCACATAAAACATAGAATGATC'
+
+inverted_mapping = dict(zip(mapping.values(), mapping.keys()))
+
+def decode_dna(decode_string):
+    pieces = []
+    for i in range(0, len(decode_string), 3):
+        piece = decode_string[i:i+3]
+        pieces.append(mapping.get(piece, ''))
+    return "".join(pieces)
+
+def encode_dna(encode_string):
+    pieces = []
+    for i in encode_string:
+        piece = inverted_mapping.get(i, '')
+        pieces.append(piece)
+    return "".join(pieces)
+
+decoded_sequence = decode_dna(decode_string)
+print('Decoded String is:', decoded_sequence)
